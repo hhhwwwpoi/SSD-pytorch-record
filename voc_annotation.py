3827cacdf6c0aeba4c72ce9baa3ed270fe1d0432
hhhwwwpoi <1569358483@qq.com>
2021-07-25 23:43:00 +0800

    ssd.py train.py predict.py voc_annotation.py voc2ssd.py vision_for_prior.py

---
+++ voc_annotation.py
@@ -0,0 +1,44 @@
+#---------------------------------------------#
+#   运行前一定要修改classes
+#   如果生成的2007_train.txt里面没有目标信息
+#   那么就是因为classes没有设定正确
+#---------------------------------------------#
+import xml.etree.ElementTree as ET
+from os import getcwd
+
+sets=[('2007', 'trainval'), ('2007', 'test')]
+
+classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+
+def convert_annotation(year, image_id, list_file,image_set):
+    in_file = open('VOC%s_06-Nov-%s/Annotations/%s.xml'%(image_set, year, image_id), encoding='utf-8')
+    tree=ET.parse(in_file)
+    root = tree.getroot()
+
+    for obj in root.iter('object'):
+        difficult = 0 
+        if obj.find('difficult')!=None:
+            difficult = obj.find('difficult').text
+            
+        cls = obj.find('name').text
+        if cls not in classes or int(difficult)==1:
+            continue
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
+        #通过指定字符连接列表中的内容,形成字符串 eg: ','.join([1,2,3,4])  ==  '1,2,3,4'
+        list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
+
+wd = getcwd()
+
+for year, image_set in sets:
+
+        image_ids = open('VOC%s_06-Nov-%s/ImageSets/Main/%s.txt'%(image_set, year, image_set)).read().strip().split()
+        list_file = open('%s_%s.txt'%(year, image_set), 'w')
+        for image_id in image_ids:
+            list_file.write('%s/VOC%s_06-Nov-%s/JPEGImages/%s.jpg'%(wd, image_set, year, image_id))
+            convert_annotation(year, image_id, list_file,image_set)
+            list_file.write('\n')
+        list_file.close()
+
+
